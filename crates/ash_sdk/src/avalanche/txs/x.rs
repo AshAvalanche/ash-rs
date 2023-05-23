a275843f1e920df321be0cb2c1ae4d5a58f647f3
Gauthier Leonard <gauthier@e36knots.com>
2023-05-23 18:02:49 +0200

    feat(sdk): add import/export to X from P

--- crates/ash_sdk/src/avalanche/txs/x.rs
+++ crates/ash_sdk/src/avalanche/txs/x.rs
@@ -6,7 +6,7 @@
 use crate::{avalanche::wallets::AvalancheWallet, errors::*};
 use avalanche_types::{
     ids::{short::Id as ShortId, Id},
-    wallet::x::transfer,
+    wallet::x::{export, transfer},
 };
 
 /// Transfer AVAX from a wallet to the receiver
@@ -31,6 +31,29 @@ pub async fn transfer_avax(
     Ok(tx_id)
 }
 
+/// Export AVAX to another chain (from the X-Chain)
+// See https://github.com/ava-labs/avalanche-types-rs/blob/0f499e038ca01af09c5be207b6d144262222e659/src/wallet/p/import.rs#L98
+pub async fn export_avax(
+    wallet: &AvalancheWallet,
+    dest_chain_id: Id,
+    amount: u64,
+    check_acceptance: bool,
+) -> Result<Id, AshError> {
+    let tx_id = export::Tx::new(&wallet.xchain_wallet.x())
+        .destination_blockchain_id(dest_chain_id)
+        .amount(amount)
+        .check_acceptance(check_acceptance)
+        .issue()
+        .await
+        .map_err(|e| AvalancheWalletError::IssueTx {
+            blockchain_name: "X-Chain".to_string(),
+            tx_type: "export".to_string(),
+            msg: e.to_string(),
+        })?;
+
+    Ok(tx_id)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,7 +62,7 @@ mod tests {
 
     const AVAX_EWOQ_PRIVATE_KEY: &str =
         "PrivateKey-ewoqjP7PxY4yr3iLTpLisriqt94hdyDFNgchSxGGztUrTXtNN";
-    const AVAX_LOCAL_XCHAIN_ADDR: &str = "X-custom1w44zzlzf68gwaskce2s4r82t5u08pje5mhq2en";
+    const AVAX_DEST_XCHAIN_ADDR: &str = "X-custom1w44zzlzf68gwaskce2s4r82t5u08pje5mhq2en";
 
     // Load the test network using avalanche-network-runner
     fn load_test_network() -> AvalancheNetwork {
@@ -54,18 +77,18 @@ mod tests {
             .create_wallet_from_cb58(AVAX_EWOQ_PRIVATE_KEY)
             .unwrap();
         let rpc_url = &local_network.get_xchain().unwrap().rpc_url;
-        let init_balance = get_balance(rpc_url, AVAX_LOCAL_XCHAIN_ADDR, "AVAX").unwrap();
+        let init_balance = get_balance(rpc_url, AVAX_DEST_XCHAIN_ADDR, "AVAX").unwrap();
 
         transfer_avax(
             &local_wallet,
-            address_to_short_id(AVAX_LOCAL_XCHAIN_ADDR, "X"),
+            address_to_short_id(AVAX_DEST_XCHAIN_ADDR, "X"),
             100000000,
             true,
         )
         .await
         .unwrap();
 
-        let final_balance = get_balance(rpc_url, AVAX_LOCAL_XCHAIN_ADDR, "AVAX").unwrap();
+        let final_balance = get_balance(rpc_url, AVAX_DEST_XCHAIN_ADDR, "AVAX").unwrap();
 
         assert_eq!(init_balance.balance + 100000000, final_balance.balance)
     }
