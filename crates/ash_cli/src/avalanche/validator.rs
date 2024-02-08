5074b8a0953d8b899ae42831aaf58b36ce29e242
Gauthier Leonard <gauthier@e36knots.com>
2024-02-08 16:53:07 +0100

    feat: use AddPermissionlessValidatorTx

--- crates/ash_cli/src/avalanche/validator.rs
+++ crates/ash_cli/src/avalanche/validator.rs
@@ -7,9 +7,20 @@ use crate::{
     avalanche::{wallet::*, *},
     utils::{error::CliError, parsing::*, templating::*, version_tx_cmd},
 };
-use ash_sdk::avalanche::{subnets::AvalancheSubnetType, AVAX_PRIMARY_NETWORK_ID};
+use ash_sdk::avalanche::{
+    nodes::ProofOfPossession, subnets::AvalancheSubnetType, AVAX_PRIMARY_NETWORK_ID,
+};
 use async_std::task;
-use clap::{Parser, Subcommand};
+use chrono::Utc;
+use clap::{Parser, Subcommand, ValueEnum};
+use std::fmt::Display;
+
+/// Node signer format
+#[derive(Display, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub(crate) enum SignerFormat {
+    Str,
+    Json,
+}
 
 /// Interact with Avalanche validators
 #[derive(Parser)]
@@ -45,19 +56,19 @@ enum ValidatorSubcommands {
         id: String,
         /// Validator weight (permissioned Subnet) or stake in AVAX (elastic Subnet)
         stake_or_weight: u64,
-        /// Start time of the validation (YYYY-MM-DDTHH:MM:SSZ)
+        /// Start time of the validation (YYYY-MM-DDTHH:MM:SSZ), defaults to now
         #[arg(long, short = 'S')]
-        start_time: String,
+        start_time: Option<String>,
         /// End time of the validation (YYYY-MM-DDTHH:MM:SSZ)
         #[arg(long, short = 'E')]
         end_time: String,
-        /// Delegation fee (percentage)
+        /// Delegation fee (percentage), defaults to 2%
         #[arg(long, short = 'f', default_value = "2")]
         delegation_fee: u32,
         /// Private key to sign the transaction with
         #[arg(long, short = 'p', env = "AVALANCHE_PRIVATE_KEY")]
         private_key: String,
-        /// Private key format
+        /// Private key encoding (cb58 or hex)
         #[arg(
             long,
             short = 'e',
@@ -65,6 +76,13 @@ enum ValidatorSubcommands {
             env = "AVALANCHE_KEY_ENCODING"
         )]
         key_encoding: PrivateKeyEncoding,
+        /// Signer (BLS public key and PoP) in "public_key:PoP" or JSON format
+        /// (e.g. '{"publicKey":"public_key","proofOfPossession":"pop"}')
+        #[arg(long, short = 'B')]
+        signer: Option<String>,
+        /// Signer format (str or json)
+        #[arg(long, short = 'F', default_value = "str")]
+        signer_format: SignerFormat,
         /// Whether to wait for transaction acceptance
         #[arg(long, short = 'w')]
         wait: bool,
@@ -176,18 +194,43 @@ fn add(
     subnet_id: &str,
     id: &str,
     stake_or_weight: u64,
-    start_time: String,
+    start_time: Option<String>,
     end_time: String,
     delegation_fee: u32,
     private_key: &str,
     key_encoding: PrivateKeyEncoding,
+    signer: Option<String>,
+    signer_format: SignerFormat,
     wait: bool,
     config: Option<&str>,
     json: bool,
 ) -> Result<(), CliError> {
     let node_id_parsed = parse_node_id(id)?;
-    let start_time_parsed = parse_datetime(&start_time)?;
+    let start_time_parsed = match start_time {
+        Some(start_time) => parse_datetime(&start_time)?,
+        None => Utc::now(),
+    };
     let end_time_parsed = parse_datetime(&end_time)?;
+    let signer_parsed = match signer.clone() {
+        Some(signer_str) => match signer_format {
+            SignerFormat::Str => {
+                let parts: Vec<&str> = signer_str.split(':').collect();
+                if parts.len() != 2 {
+                    return Err(CliError::dataerr(
+                        "Signer must be in the format 'public_key:PoP'".to_string(),
+                    ));
+                }
+                serde_json::from_value::<ProofOfPossession>(serde_json::json!({
+                    "publicKey": parts[0],
+                    "proofOfPossession": parts[1]
+                }))
+                .map_err(|e| CliError::dataerr(format!("Error parsing signer: {e}")))?
+            }
+            SignerFormat::Json => serde_json::from_str(&signer_str)
+                .map_err(|e| CliError::dataerr(format!("Error parsing signer: {e}")))?,
+        },
+        None => ProofOfPossession::default(),
+    };
 
     let mut network = load_network(network_name, config)?;
     update_network_subnets(&mut network)?;
@@ -204,14 +247,19 @@ fn add(
     let validator = match subnet.subnet_type {
         AvalancheSubnetType::PrimaryNetwork => task::block_on(async {
             subnet
-                .add_avalanche_validator(
+                .add_validator_permissionless(
                     &wallet,
                     node_id_parsed,
+                    subnet.id,
                     // Multiply by 1 billion to convert from AVAX to nAVAX
                     stake_or_weight * 1_000_000_000,
                     start_time_parsed,
                     end_time_parsed,
                     delegation_fee,
+                    match signer {
+                        Some(_) => Some(signer_parsed),
+                        None => None,
+                    },
                     wait,
                 )
                 .await
@@ -261,6 +309,8 @@ pub(crate) fn parse(
             delegation_fee,
             private_key,
             key_encoding,
+            signer,
+            signer_format,
             wait,
         } => add(
             &validator.network,
@@ -272,6 +322,8 @@ pub(crate) fn parse(
             delegation_fee,
             &private_key,
             key_encoding,
+            signer,
+            signer_format,
             wait,
             config,
             json,
